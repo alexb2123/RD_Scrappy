206de5937cbe45eabc97659ab708623aa8808e4d
Alexandru Bordei <alexb2123@gmail.com>
2020-04-05 18:04:50 -0400

    scrape multiple pages, need to implement multiple url

--- spiders/webstaraunt.py
+++ spiders/webstaraunt.py
@@ -1,23 +1,32 @@
-# -*- coding: utf-8 -*-
 import scrapy
 
 
-class ItemSpider(scrapy.Spider):
-    name = 'Quotes'
-#    allowed_domains = ['https://www.webstaurantstore.com']
-
-    def start_requests(self):
-        start_urls = ['https://www.webstaurantstore.com/choice-23-x-13-led-oval-open-sign-with-two-display-modes/466OPECONOVL.html'
-                      'https://www.webstaurantstore.com/choice-23-x-13-led-oval-pizza-sign-with-two-display-modes/466LEDPIZOVL.html'
-                      'https://www.webstaurantstore.com/choice-23-x-13-led-oval-coffee-sign-with-2-display-modes/466LEDCOFOVL.html']
-
-        for url in start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+class PostsSpider(scrapy.Spider):
+    name = "products"
+    start_urls = [
+        # 'https://www.webstaurantstore.com/10259/ice-buckets.html?vendor=American-Metalcraft',
+        # 'https://www.webstaurantstore.com/3087/food-storage-containers.html?vendor=Cambro'
+        'https://www.webstaurantstore.com/51003/knives.html'
+    ]
 
     def parse(self, response):
-        page = response.url.split("/")[2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        # find the product listing div
+        product_listing = response.xpath('//*[@id="product_listing"]')
+        # for each product extract data
+        for product in product_listing.xpath('//*[@class="ag-item gtm-product "]'):
+            yield {
+                'brand': product.attrib['data-brand'],
+                'category': product.attrib['data-list'],
+                'description': product.attrib['data-description'],
+                'price': product.attrib['data-price'],
+                'vendor_item_number': product.attrib['data-item-number'][3:],
+                'websta_item_number': product.attrib['data-item-number'],
+                'url': product.xpath('.//div[1]/a[2]').attrib['href']
+            }
 
+        # look for the next page button, get url
+        next_page = response.css('a[rel="next"]::attr(href)').get()
+
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
